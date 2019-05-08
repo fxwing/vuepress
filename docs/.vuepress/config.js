467a7e5bee21aa56e880151970db5c7677737d49
fengxiongwei <89005676@sfmail.sf-express.com>
2019-05-08 15:45:47 +0800

    :beers: 新建vuepress

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  title: "前端蜗牛路",
-  description: "郭邯同学的个人博客",
+  title: "_theFeng",
+  description: "fxw同学的个人博客",
   head: [
     ['link', {
       rel: 'icon',
@@ -11,6 +11,7 @@ module.exports = {
     lineNumbers: true
   },
   themeConfig: {
+    sidebar: 'auto',
     nav: [{
         text: '首页',
         link: '/'
@@ -29,38 +30,29 @@ module.exports = {
       },
       {
         text: '掘金',
-        link: 'https://juejin.im/user/5b0f41de518825153a440dd9'
+        link: ''
       },
       {
         text: 'GitHub',
-        link: 'https://github.com/ggwork'
+        link: 'https://github.com/fxwing'
       },
       {
         text: '知乎',
-        link: 'https://www.zhihu.com/people/yagb/activities'
+        link: ''
       },
-      {
-        text: '女朋友',
-        link: '/girlfriend/'
-      }
     ],
     sidebar: {
-      '/softskill/': [
-        '',
-        '软技能-代码之外的生存指南1（职业篇）',
-        '软技能-代码之外的生存指南2（自我营销篇）',
-        '软技能-代码之外的生存指南3（自我学习）',
-        '软技能-代码之外的生存指南4（生产力）',
-        '软技能-代码之外的生存指南5（理财）',
-        '软技能-代码之外的生存指南6（健身）',
-        '软技能-代码之外的生存指南7（精神）'
-      ],
       '/webgl/': [
         '',
         '1-什么是webgl',
         '2-初识webgl和着色器',
         '3-坐标系与鼠标交互'
-      ]
+      ],
+      // '/': [
+      //   '',
+      //   '侧标导航1',
+      //   '侧标导航1'
+      // ],
     },
     sidebarDepth: 2,
     lastUpdated: 'Last Updated',
